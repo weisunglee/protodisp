472d28185fc8fcf2dcd41f3ef5498070be452a75
vincent <vincent.lee@isapsolution.com>
2018-12-19 17:34:52 +0800

    implement message serializer

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,16 +1,18 @@
 #include "demo.pb.h"
 #include <iostream>
+#include "message_serializer.h"
 
 int main()
 {
   demo::demo_message_int dmi;
   dmi.set_data(100);
-  
+  std::cout << dmi.data() << std::endl;
+  std::string dmi_serialized = proto::message_serializer::serialize(dmi);
+    
   demo::demo_message_string dms;
   dms.set_data("hello world");
-
-  std::cout << dmi.data() << std::endl;
-  std::cout << dms.data() << std::endl;
+  std::cout << dms.data() << std::endl;  
+  std::string dms_serialized = proto::message_serializer::serialize(dms);
     
   return 0;
 }
---
+++ src/message_serializer.h
@@ -0,0 +1,35 @@
+#ifndef MESSAGE_SERIALIZER_H_
+#define MESSAGE_SERIALIZER_H_
+
+#include <string>
+#include "proto_message.pb.h"
+
+namespace proto {
+    class message_serializer {
+    public:
+        template<typename T>
+        static std::string serialize(T&& message);
+
+    private:
+        template<typename T>
+        static std::string serialize(T&& message, const std::string& full_name);
+
+        static std::string wrap(std::string&& buf, const std::string& full_name);        
+    };
+
+    template<typename T>
+    std::string message_serializer::serialize(T&& message) {
+        return message_serializer::serialize(message, message.descriptor()->full_name());
+    }
+
+    template<typename T>
+    std::string message_serializer::serialize(T&& message, const std::string& full_name) {
+        std::string buf;
+        message.SerializeToString(&buf);
+
+        return wrap(std::move(buf), full_name);
+    }
+
+} // proto
+
+#endif // MESSAGE_SERIALIZER_H_
